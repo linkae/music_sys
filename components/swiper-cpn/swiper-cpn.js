6dcc201decfd5724442650c4cb86bc8086bc8df2
muzix <49109505+linkae@users.noreply.github.com>
2023-01-30 12:17:03 +0800

    Add files via upload

---
+++ components/swiper-cpn/swiper-cpn.js
@@ -0,0 +1,28 @@
+// components/swiper-cpn.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        imgUrl:[
+            "https://p1.music.126.net/8NPIwSOiCPvTgoJQFnPxxA==/109951167885427444.jpg",
+            "https://p1.music.126.net/iZ6jTfywfhmmUG5BF96n5Q==/109951167885414367.jpg",
+            "https://p1.music.126.net/aZpRhGv2TRXNgCvDA-TxPA==/109951167885435357.jpg",
+            "https://p1.music.126.net/1HWOZBdm3dJOldCtq8ab9A==/109951167885443259.jpg"
+          ],
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
